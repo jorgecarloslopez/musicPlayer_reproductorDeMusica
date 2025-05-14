f65f749af57847307ffe4f342badc01e23a4a7fa
jorgecarloslopez <lopez.cs.jorge@gmail.com>
2025-05-13 22:42:47 -0300

    working at the DOM

---
+++ index.js
@@ -0,0 +1,8 @@
+const tituloCancion = document.querySelector(".reproductor_Musica h1");
+const nombreArtista = document.querySelector(".reproductor_Musica p");
+
+const progreso = document.getElementById("progreso");
+const cancion = document.getElementById("cancion");
+
+const botonAtras = document.getElementById(".controles button.atras");
+const botonAdelante = document.getElementById(".controles button.adelante");
