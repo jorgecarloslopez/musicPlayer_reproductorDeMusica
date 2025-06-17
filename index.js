19c9cc1f2eb0c87c6ffd84c0c4d9a82016f89005
jorgecarloslopez <lopez.cs.jorge@gmail.com>
2025-06-16 22:22:00 -0300

    working at the fuction

--- index.js
+++ index.js
@@ -15,8 +15,23 @@ const botonAdelante = document.getElementById(".controles button.adelante");
 const canciones = [
   {
     titulo: "agony",
-    nombre: "DG",
-    fuente: "music/agony remixFINAL MIX222.wav",
+    nombre: "yung_lean",
+    fuente: "music/agony_-_yung_lean_instrumental_slowed.mp3",
+  },
+  {
+    titulo: "Moonlight_on_the_River",
+    nombre: "Mac_Demarco",
+    fuente: "music/Moonlight_on_the_River_-_Mac_Demarco_Slowed.mp3",
+  },
+  {
+    titulo: "Gladiator",
+    nombre: "Now_We_Are_Free",
+    fuente: "music/Now_We_Are_Free_-_Gladiator.mp3",
+  },
+  {
+    titulo: "Dreamscape",
+    nombre: "009_Sound_System",
+    fuente: "music/Trance_-_009_Sound_System_Dreamscape.mp3",
   },
 ];
 let indiceCancionActual = 0;
@@ -32,8 +47,6 @@ botonReproducirPausar.addEventListener("click", reproducirPausar);
 function reproducirPausar() {
   if (cancion.paused) {
     reproducirCancion();
-    iconoControl.classList.add("bi-pause-fill");
-    iconoControl.classList.remove("bi-play-fill");
   } else {
     pausarCancion();
     iconoControl.classList.remove("bi-pause-fill");
@@ -43,10 +56,14 @@ function reproducirPausar() {
 
 function reproducirCancion() {
   cancion.play();
+  iconoControl.classList.add("bi-pause-fill");
+  iconoControl.classList.remove("bi-play-fill");
 }
 
 function pausarCancion() {
   cancion.pause();
+  iconoControl.classList.remove("bi-pause-fill");
+  iconoControl.classList.add("bi-play-fill");
 }
 
 actualizarInfoCancion();
